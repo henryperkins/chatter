e4bd532144bc7c2c8000e326a6a5dfd7ba2ce065
Henry Perkins <htperkins@gmail.com>
2025-01-27 07:18:17 +0000

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
     './templates/**/*.html',
     './templates/**/*.jinja2',
     './static/js/**/*.js',
+    './static/css/**/*.css', // Ensure this path includes your input.css
   ],
-
   theme: {
     extend: {
       colors: {
@@ -16,21 +16,20 @@ module.exports = {
         '32': '8rem',  // For pb-32 class
       },
       screens: {
-        '2xl': '1440px',
+        '2xl': '1440px', // Custom 2xl breakpoint
       },
       translate: {
-        '-full': '-100%',
+        '-full': '-100%', // Custom translate value
       },
       zIndex: {
-        'dropdown': '1000',
-        'sticky': '1020',
-        'fixed': '1030',
+        dropdown: '1000',
+        sticky: '1020',
+        fixed: '1030',
         'modal-backdrop': '1040',
-        'modal': '1050',
-        'popover': '1060',
-        'tooltip': '1070',
+        modal: '1050',
+        popover: '1060',
+        tooltip: '1070',
       },
-      // Add typography configuration here instead of duplicate theme
       typography: {
         DEFAULT: {
           css: {
@@ -52,19 +51,31 @@ module.exports = {
             'code::after': false,
             'pre code::before': false,
             'pre code::after': false,
-          }
-        }
-      }
+          },
+        },
+      },
+      // Extend variants under theme.extend
+      backgroundColor: ['dark'],
+      textColor: ['dark'],
+      opacity: ['disabled'],
+      scale: ['hover', 'focus'],
+      display: ['group-hover'],
+      transform: ['hover', 'focus'],
     },
-    display: ['responsive', 'group-hover'],
-    opacity: ['responsive', 'hover', 'focus', 'group-hover'],
-    transform: ['responsive', 'hover', 'focus'],
   },
-
+  corePlugins: {
+    float: false,
+    clear: false,
+    overscrollBehavior: false,
+    boxDecorationBreak: false,
+    mixBlendMode: false,
+    isolation: false,
+    tableLayout: false,
+    // 'transform' is enabled by default
+  },
   darkMode: 'class',
-
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
-  ]
-};
+  ],
+}
