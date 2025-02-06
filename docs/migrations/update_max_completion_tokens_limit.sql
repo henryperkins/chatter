35515322aae25819f2913beca700b3e13c990d41
Henry Perkins <htperkins@gmail.com>
2025-02-06 16:28:29 -0600

    up

--- docs/migrations/update_max_completion_tokens_limit.sql
+++
@@ -1,9 +0,0 @@
--- Update max_completion_tokens constraint for o1-preview models
-ALTER TABLE models DROP CONSTRAINT IF EXISTS models_max_completion_tokens_check;
-ALTER TABLE models ADD CONSTRAINT models_max_completion_tokens_check 
-    CHECK (max_completion_tokens >= 1 AND max_completion_tokens <= 25000);
-
--- Update any existing o1-preview models that might have been limited by the old constraint
-UPDATE models
-SET max_completion_tokens = LEAST(max_completion_tokens, 25000)
-WHERE model_type = 'o1-preview' AND requires_o1_handling = true;
